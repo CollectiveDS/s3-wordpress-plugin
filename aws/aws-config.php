13251b18dc85c85dd94ba9e1c08e95ecf0c30b97
hanzenlim <hanzenlim@gmail.com>
2013-11-06 11:14:31 -0800

    Add aws-config.php with empty params

---
+++ aws/aws-config.php
@@ -0,0 +1,11 @@
+<?php return array(
+    'includes' => array('_aws'),
+    'services' => array(
+        'default_settings' => array(
+            'params' => array(
+                'key' => 'your-api-key',
+                'secret' => 'your-secret-key',
+            )
+        )
+    )
+);
